ddd3a6e31451a81fbf86b2114082bec5d33b145a
Tim Fuqua <timfuqua@gmail.com>
2018-08-23 19:53:29 -0700

    Removing ViewController stub. Replacing with TodoListViewController. This is the main landing page screen.

---
+++ SBGTodoListMVC/SBGTodoListMVC/Controllers/TodoListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TodoListViewController.swift
+//  SBGTodoListMVC
+//
+//  Created by Tim Fuqua on 8/23/18.
+//  Copyright © 2018 SBGCodeTest. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK:- TodoListViewController
+class TodoListViewController: UIViewController {
+
+
+}
+
+
+// MARK:- life cycle
+extension TodoListViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
