e2d34d10a7dcc88816784a7ff01ddba81a97b539
Tim Fuqua <timfuqua@gmail.com>
2018-08-25 13:34:37 -0700

    Adding remove and removeAll unit tests and functions to the TodoTaskManager.

--- SBGTodoListMVC/SBGTodoListMVC/Model/TodoTaskManager.swift
+++ SBGTodoListMVC/SBGTodoListMVC/Model/TodoTaskManager.swift
@@ -17,31 +17,50 @@ class TodoTaskManager {
     private var allTasks: [NSManagedObject] = []
     
     // MARK: CoreData
-    var appDelegate: AppDelegate? {
+    private var appDelegate: AppDelegate? {
         return UIApplication.shared.delegate as? AppDelegate
     }
     
-    var managedContext: NSManagedObjectContext? {
+    private var managedContext: NSManagedObjectContext? {
         return appDelegate?.persistentContainer.viewContext
     }
     
-    var todoTaskEntity: NSEntityDescription? {
+    private var todoTaskEntity: NSEntityDescription? {
         guard let managedContext = managedContext else { return nil }
         return NSEntityDescription.entity(forEntityName: "TodoTask", in: managedContext)
     }
     
+    private let todoFetchRequestForFetch = NSFetchRequest<NSManagedObject>(entityName: "TodoTask")
+    private let todoFetchRequestForRemove = NSFetchRequest<NSFetchRequestResult>(entityName: "TodoTask")
+    private var deleteAllRequest: NSBatchDeleteRequest {
+        return NSBatchDeleteRequest(fetchRequest: todoFetchRequestForRemove)
+    }
+
 }
 
 
 // MARK:- initialize
 extension TodoTaskManager {
-    
+    func fetchAllTasks() {
+        guard let managedContext = managedContext else { return }
+        
+        do {
+            let fetchedTasks = try managedContext.fetch(todoFetchRequestForFetch)
+            allTasks = fetchedTasks
+        } catch let error as NSError {
+            debugLog("\(error)")
+        }
+    }
 }
 
 
 // MARK:- get
 extension TodoTaskManager {
-    func getAllTasks() -> [NSManagedObject] {
+    func getAllTasks(afterFetch fetch: Bool = true) -> [NSManagedObject] {
+        if fetch {
+            fetchAllTasks()
+        }
+        
         return allTasks
     }
 }
@@ -55,12 +74,47 @@ extension TodoTaskManager {
         let addedTask = NSManagedObject(entity: todoTaskEntity, insertInto: managedContext)
         
         addedTask.setValue("Test", forKey: "title")
-        allTasks.append(addedTask)
+        
+        do {
+            try managedContext.save()
+            allTasks.append(addedTask)
+        } catch let error as NSError {
+            debugLog("\(error)")
+        }
     }
 }
 
 
 // MARK:- remove
 extension TodoTaskManager {
+    func removeTask() -> Bool {
+        guard let managedContext = managedContext else { return false }
+        
+        do {
+            let fetchedTasks = try managedContext.fetch(todoFetchRequestForFetch)
+            
+            if let firstTask = fetchedTasks.first {
+                managedContext.delete(firstTask)
+                allTasks.removeFirst()
+            }
+            
+            return true
+        } catch let error as NSError {
+            debugLog("\(error)")
+            return false
+        }
+    }
     
+    func removeAllTasks() {
+        guard let managedContext = managedContext else { return }
+        let deleteAllRequest = NSBatchDeleteRequest(fetchRequest: todoFetchRequestForRemove)
+        
+        do {
+            try managedContext.execute(deleteAllRequest)
+            try managedContext.save()
+            allTasks.removeAll()
+        } catch let error as NSError {
+            debugLog("\(error)")
+        }
+    }
 }
--- SBGTodoListMVC/SBGTodoListMVCTests/TodoTaskManagerTests.swift
+++ SBGTodoListMVC/SBGTodoListMVCTests/TodoTaskManagerTests.swift
@@ -23,7 +23,24 @@ class TodoTaskManagerTests: XCTestCase {
     func testInitializeEmpty() {
         let todoTaskManager = TodoTaskManager()
         
-        XCTAssertTrue(todoTaskManager.getAllTasks().isEmpty, "The TodoTaskManager initially has no tasks")
+        XCTAssertTrue(todoTaskManager.getAllTasks(afterFetch: false).isEmpty, "The TodoTaskManager initially has no tasks")
+    }
+    
+    func testRemoveAllEmpty() {
+        let todoTaskManager = TodoTaskManager()
+        
+        todoTaskManager.removeAllTasks()
+        let afterInitialRemoveAllTaskCount = todoTaskManager.getAllTasks().count
+        XCTAssertTrue(afterInitialRemoveAllTaskCount == 0, "The TodoTaskManager initially has no tasks")
+        
+        todoTaskManager.addTask()
+        todoTaskManager.addTask()
+        let afterAddTaskCount = todoTaskManager.getAllTasks().count
+        XCTAssertTrue(afterAddTaskCount != 0, "The TodoTaskManager should have more than 0 tasks")
+
+        todoTaskManager.removeAllTasks()
+        let afterFinalRemoveAllTaskCount = todoTaskManager.getAllTasks().count
+        XCTAssertTrue(afterFinalRemoveAllTaskCount == 0, "The TodoTaskManager initially has no tasks")
     }
     
     func testAddIncreasesCountByOne() {
@@ -35,6 +52,25 @@ class TodoTaskManagerTests: XCTestCase {
         todoTaskManager.addTask()
         let afterAddTaskCount = todoTaskManager.getAllTasks().count
         XCTAssertTrue(afterAddTaskCount == initialTaskCount + 1, "The TodoTaskManager should have one more task than before")
+    }
+    
+    func testRemoveDecreasesCountByOne() {
+        let todoTaskManager = TodoTaskManager()
+        
+        let initialTaskCount = todoTaskManager.getAllTasks().count
+        XCTAssertTrue(initialTaskCount == 0, "The TodoTaskManager initially has no tasks")
+        
+        todoTaskManager.addTask()
+        let afterAddTaskCount = todoTaskManager.getAllTasks().count
+        XCTAssertTrue(afterAddTaskCount == initialTaskCount + 1, "The TodoTaskManager should have one more task than before")
+        
+        let removeResult = todoTaskManager.removeTask()
+        let afterRemoveTaskCount = todoTaskManager.getAllTasks().count
+        XCTAssertTrue(removeResult, "The remove operation should succeed")
+        XCTAssertTrue(afterRemoveTaskCount == afterAddTaskCount - 1, "The TodoTaskManager should have one less task than before")
+        
+        let finalTaskCount = todoTaskManager.getAllTasks().count
+        XCTAssertTrue(finalTaskCount == initialTaskCount, "The TodoTaskManager should have no tasks after add and remove")
     }
     
 }
